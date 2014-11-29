b93be9611755dd3b356c810dcbfc6047d3460b84
Ione Souza Junior <junior@ionixjunior.com.br>
2014-11-28 23:40:14 -0200

    Removido arquivos obsoletos. Ajustando telas principais

--- www/js/app.js
+++ www/js/app.js
@@ -30,43 +30,24 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       controller: 'AppCtrl'
     })
 
-    .state('app.search', {
-      url: "/search",
+    .state('app.movimentacao', {
+      url: "/movimentacao",
       views: {
         'menuContent' :{
-          templateUrl: "templates/search.html"
+          templateUrl: "templates/movimentacao.html"
         }
       }
     })
 
-    .state('app.browse', {
-      url: "/browse",
+    .state('app.configuracao', {
+      url: "/configuracao",
       views: {
         'menuContent' :{
-          templateUrl: "templates/browse.html"
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
+          templateUrl: "templates/configuracao.html"
         }
       }
     });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/movimentacao');
 });
 
