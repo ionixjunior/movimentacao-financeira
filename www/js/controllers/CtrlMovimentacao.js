8484bd984308bf238ca56fd45f97a9416dc05897
Ione Souza Junior <junior@ionixjunior.com.br>
2014-12-20 20:19:55 -0200

    Implementado filtro de carregamento de saldo e últimas movimentações apenas até o dia atual

--- www/js/controllers/CtrlMovimentacao.js
+++ www/js/controllers/CtrlMovimentacao.js
@@ -3,9 +3,10 @@ angular.module('starter.controllers').controller('CtrlMovimentacao', ['$scope', 
 	$scope.saldo = Number(0);
 
 	$scope.carregaSaldo = function() {
-	var query = 'SELECT (SELECT SUM(valor) FROM movimento WHERE tipo_movimento = ? AND situacao = ?) AS receita, (SELECT SUM(valor) FROM movimento WHERE tipo_movimento = ? AND situacao = ?) AS despesa FROM movimento';
+	var query = 'SELECT (SELECT SUM(valor) FROM movimento WHERE tipo_movimento = ? AND situacao = ? AND data <= ?) AS receita, (SELECT SUM(valor) FROM movimento WHERE tipo_movimento = ? AND situacao = ? AND data <= ?) AS despesa FROM movimento';
+	var data = new Date();
 
-	$cordovaSQLite.execute(db, query, [2, 'A', 1, 'A'])
+	$cordovaSQLite.execute(db, query, [2, 'A', data, 1, 'A', data])
 	  .then(function(dados) {
 	    if (dados.rows.length > 0) {
 	      $scope.saldo = Number(dados.rows.item(0)['receita']) - Number(dados.rows.item(0)['despesa']);
@@ -16,8 +17,8 @@ angular.module('starter.controllers').controller('CtrlMovimentacao', ['$scope', 
 	};
 
 	$scope.carregaUltimasMovimentacoes = function() {
-	var query = 'SELECT movimento.codigo, movimento.valor, movimento.tipo_movimento, movimento.data, categoria_movimento.nome FROM movimento INNER JOIN categoria_movimento ON (categoria_movimento.codigo = movimento.categoria_codigo) WHERE movimento.situacao = ? ORDER BY movimento.data DESC, movimento.codigo DESC LIMIT 10';
-	$cordovaSQLite.execute(db, query, ['A'])
+	var query = 'SELECT movimento.codigo, movimento.valor, movimento.tipo_movimento, movimento.data, categoria_movimento.nome FROM movimento INNER JOIN categoria_movimento ON (categoria_movimento.codigo = movimento.categoria_codigo) WHERE movimento.situacao = ? AND data <= ? ORDER BY movimento.data DESC, movimento.codigo DESC LIMIT 10';
+	$cordovaSQLite.execute(db, query, ['A', new Date()])
 	  .then(function(dados) {
 	    for (var i = 0; i < dados.rows.length; i++) {
 	      $scope.movimentacao.push(dados.rows.item(i));
