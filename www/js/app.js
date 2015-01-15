51a4d4ac0dca199380b45300aa9feecec2c39a71
Ione Souza Junior <junior@ionixjunior.com.br>
2015-01-14 22:13:30 -0200

    Ajustado carregamento do teclado

--- www/js/app.js
+++ www/js/app.js
@@ -13,7 +13,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
     }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
